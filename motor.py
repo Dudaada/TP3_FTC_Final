e30795b13163f4594d53d08df4da3e3f1219d04e
helenasperez <helena.perez@ufv.br>
2026-06-14 20:39:06 -0300

    Add functions to create transition map and process words

--- motor.py
+++ motor.py
@@ -1,1 +1,23 @@
+def criar_mapa_transicoes(afd):
+    mapa = {}
+    for transicao in afd.transicoes:
+        origem = transicao["origem"]
+        simbolo = transicao["simbolo"]
+        destino = transicao["destino"]
+        if origem not in mapa:
+            mapa[origem] = {}
+        mapa[origem][simbolo] = destino
+      return mapa
 
+
+def processar_palavra(afd, mapa_transicoes, palavra):
+    estado_atual = afd.estado_inicial
+    for simbolo in palavra:
+        if simbolo not in ('0', '1'):
+            return False
+        if estado_atual not in mapa_transicoes:
+            return False
+        if simbolo not in mapa_transicoes[estado_atual]:
+            return False
+        estado_atual = mapa_transicoes[estado_atual][simbolo]
+    return estado_atual in afd.estados_finais
