efe9f7b2f0e16918dacb83dd8cb9534dde8e4daf
helenasperez <helena.perez@ufv.br>
2026-06-14 20:43:06 -0300

    Simplify transition checks in processar_palavra
    Refactor processar_palavra function to simplify transition checks.

--- motor.py
+++ motor.py
@@ -9,15 +9,13 @@ def criar_mapa_transicoes(afd):
         mapa[origem][simbolo] = destino
     return mapa
 
-
 def processar_palavra(afd, mapa_transicoes, palavra):
     estado_atual = afd.estado_inicial
+    
     for simbolo in palavra:
-        if simbolo not in ('0', '1'):
+        if estado_atual in mapa_transicoes and simbolo in mapa_transicoes[estado_atual]:
+            estado_atual = mapa_transicoes[estado_atual][simbolo]
+        else:
             return False
-        if estado_atual not in mapa_transicoes:
-            return False
-        if simbolo not in mapa_transicoes[estado_atual]:
-            return False
-        estado_atual = mapa_transicoes[estado_atual][simbolo]
+            
     return estado_atual in afd.estados_finais
