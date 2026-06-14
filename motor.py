48bacea89874e37ccc24a59de8ed0c2d19f1c2d6
helenasperez <helena.perez@ufv.br>
2026-06-14 20:47:19 -0300

    Refactor processar_palavra function for clarity

--- motor.py
+++ motor.py
@@ -11,11 +11,10 @@ def criar_mapa_transicoes(afd):
 
 def processar_palavra(afd, mapa_transicoes, palavra):
     estado_atual = afd.estado_inicial
-    
     for simbolo in palavra:
-        if estado_atual in mapa_transicoes and simbolo in mapa_transicoes[estado_atual]:
-            estado_atual = mapa_transicoes[estado_atual][simbolo]
-        else:
+        if estado_atual not in mapa_transicoes:
             return False
-            
+        if simbolo not in mapa_transicoes[estado_atual]:
+            return False
+        estado_atual = mapa_transicoes[estado_atual][simbolo]
     return estado_atual in afd.estados_finais
